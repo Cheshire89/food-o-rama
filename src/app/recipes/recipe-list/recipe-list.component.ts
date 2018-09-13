0af3851ed83d203a13302562f02f75fb82923587
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2018-09-12 18:55:13 -0600

    init recepie app init markup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe', 
+      'This is a test recipe', 
+      'https://www.goodfood.com.au/content/dam/images/h/0/f/a/q/i/image.related.wideLandscape.940x529.h0fa4n.png/1515456591895.jpg'),
+    new Recipe(
+      'Sushi!', 
+      'Om nom nom', 
+      'https://staybarcelonaapartments.com/blog/wp-content/uploads/2016/12/6x4-rolls-monster-sushi-barcelona.jpg'),
+  ];
   constructor() { }
 
   ngOnInit() {
