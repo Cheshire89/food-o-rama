d7cea3f57fc9ace80ec0c712c566eaa111b1b44e
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2018-11-25 18:37:12 -0700

    user router module + clean up old routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<string>();
-  onSelect(activeTab: string) {
-    this.featureSelected.emit(activeTab);
-  }
+export class HeaderComponent {
   constructor() { }
-  ngOnInit() {}
-
 }
