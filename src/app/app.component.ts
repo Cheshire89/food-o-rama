08bfd069f6e85b9c2142d2654bb6c3e7d745f860
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2018-11-30 16:43:30 -0700

    wire up edit / new recepies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  activatedTab: string = 'recipe';
-  
-  onNavigate(activeTab: string) {
-    this.activatedTab = activeTab
-  }
+  constructor(){}
 }
