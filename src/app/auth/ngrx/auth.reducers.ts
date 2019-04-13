b82dcb3b2cd6de7a4179f4b6b6a363ceb1f9fec0
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2019-04-13 17:11:40 -0600

    register auth actions in auth reducer

--- src/app/auth/ngrx/auth.reducers.ts
+++ src/app/auth/ngrx/auth.reducers.ts
@@ -12,6 +12,22 @@ const initialState: State = {
 
 export function authReducer(state = initialState, action: AuthActions.AuthActions){
     switch(action.type){
+        case AuthActions.SIGNUP:
+        case AuthActions.LOGIN:
+            return {
+                ...state,
+                authenticated: true
+            };
+        case AuthActions.LOGOUT:
+            return {
+                ...state,
+                token: null,
+                authenticated: false
+            };
+        case AuthActions.SET_TOKEN:
+            return {
+                ...state,
+            };
         default:
             return state;
     }
