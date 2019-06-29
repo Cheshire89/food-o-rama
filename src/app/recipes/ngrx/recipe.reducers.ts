0d35039cdc9b1c5084831c9c5fedda30e5897654
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2019-06-28 21:10:33 -0600

    wireup Recipe reducers

--- src/app/recipes/ngrx/recipe.reducers.ts
+++ src/app/recipes/ngrx/recipe.reducers.ts
@@ -1,4 +1,5 @@
 import { Recipe } from "src/app/shared/recipe.model";
+import * as RecipeActions from "./recipe.actions";
 
 export interface State {
     recipes: Recipe[];
@@ -12,6 +13,39 @@ const initialState: State = {
     recipes: []
 };
 
-export function recipeReducer(state = initialState, action){
-    return state;
+export function recipeReducer(state = initialState, action: RecipeActions.RecipeActions){
+    switch(action.type){
+        case (RecipeActions.SET_RECIPES):
+            return {
+                ...state,
+                recipes: [...action.payload]
+            };
+        case (RecipeActions.ADD_RECIPE):
+            return {
+                ...state,
+                recipes: [...state.recipes, action.payload]
+            };
+        case (RecipeActions.UPDATE_RECIPE):
+            const recipe = state.recipes[action.payload.index];
+            const updatedRecipe = {
+                ...recipe,
+                ...action.payload.updatedRecipe
+            };
+            const recipes = [...state.recipes];
+                    recipes[action.payload.index] = updatedRecipe;
+
+            return {
+                ...state,
+                recipes: recipes
+            };
+        case (RecipeActions.DELETE_RECIPE):
+            const oldRecipes = [...state.recipes];
+            oldRecipes.splice(action.payload, 1);
+            return {
+                ...state,
+                recipes: oldRecipes
+            };
+        default:
+            return state;
+    }
 }
